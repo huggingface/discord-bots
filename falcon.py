91e5d8fa42d6eb86ab1aea0aa4b71e4dac19197f
lunarflu <70143200+lunarflu@users.noreply.github.com>
2023-07-26 19:50:41 +0200

    Update falcon.py

--- falcon.py
+++ falcon.py
@@ -10,12 +10,8 @@ threadid_conversation = {}
 instructions = "The following is a conversation between a highly knowledgeable and intelligent AI assistant, called Falcon, and a human user, called User. In the following interactions, User and Falcon will converse in natural language, and Falcon will answer User's questions. Falcon was built to be respectful, polite and inclusive. Falcon was built by the Technology Innovation Institute in Abu Dhabi. Falcon will never decline to answer a question, and always attempts to give an answer that User would be satisfied with. It knows a lot, and always tells the truth. The conversation begins."
 falcon_client = Client("HuggingFaceH4/falcon-chat", HF_TOKEN)
 
-BOT_USER_ID = (
-    1086256910572986469 if os.getenv("TEST_ENV", False) else 1102236653545861151
-)
-FALCON_CHANNEL_ID = (
-    1079459939405279232 if os.getenv("TEST_ENV", False) else 1119313248056004729
-)
+BOT_USER_ID = 1086256910572986469 if os.getenv("TEST_ENV", False) else 1102236653545861151
+FALCON_CHANNEL_ID = 1079459939405279232 if os.getenv("TEST_ENV", False) else 1119313248056004729
 
 
 async def waitjob(job):
@@ -31,9 +27,7 @@ def falcon_initial_generation(prompt, instructions, thread):
     temperature = 0.8
     p_nucleus_sampling = 0.9
 
-    job = falcon_client.submit(
-        prompt, chathistory, instructions, temperature, p_nucleus_sampling, fn_index=1
-    )
+    job = falcon_client.submit(prompt, chathistory, instructions, temperature, p_nucleus_sampling, fn_index=1)
     while job.done() is False:
         pass
     else:
@@ -49,10 +43,7 @@ def falcon_initial_generation(prompt, instructions, thread):
         output_text = data[-1][-1]
         threadid_conversation[thread.id] = full_generation
         if len(output_text) > 1300:
-            output_text = (
-                output_text[:1300]
-                + "...\nTruncating response to 2000 characters due to discord api limits."
-            )
+            output_text = output_text[:1300] + "...\nTruncating response to 2000 characters due to discord api limits."
         if os.environ.get("TEST_ENV") == "True":
             print(output_text)
         return output_text
@@ -71,9 +62,7 @@ async def try_falcon(interaction, prompt):
                 await interaction.response.send_message("Working on it!")
                 channel = interaction.channel
                 message = await channel.send("Creating thread...")
-                thread = await message.create_thread(
-                    name=prompt, auto_archive_duration=60
-                )  # interaction.user
+                thread = await message.create_thread(name=prompt, auto_archive_duration=60)  # interaction.user
                 await thread.send(
                     "[DISCLAIMER: HuggingBot is a **highly experimental** beta feature; The Falcon model and system prompt can be found here: https://huggingface.co/spaces/HuggingFaceH4/falcon-chat]"
                 )
@@ -81,9 +70,7 @@ async def try_falcon(interaction, prompt):
                 if os.environ.get("TEST_ENV") == "True":
                     print("Running falcon_initial_generation...")
                 loop = asyncio.get_running_loop()
-                output_text = await loop.run_in_executor(
-                    None, falcon_initial_generation, prompt, instructions, thread
-                )
+                output_text = await loop.run_in_executor(None, falcon_initial_generation, prompt, instructions, thread)
                 falcon_userid_threadid_dictionary[thread.id] = interaction.user.id
 
                 await thread.send(output_text)
@@ -96,12 +83,9 @@ async def continue_falcon(message):
     try:
         if not message.author.bot:
             global falcon_userid_threadid_dictionary  # tracks userid-thread existence
-            if (
-                message.channel.id in falcon_userid_threadid_dictionary
-            ):  # is this a valid thread?
+            if message.channel.id in falcon_userid_threadid_dictionary:  # is this a valid thread?
                 if (
-                    falcon_userid_threadid_dictionary[message.channel.id]
-                    == message.author.id
+                    falcon_userid_threadid_dictionary[message.channel.id] == message.author.id
                 ):  # more than that - is this specifically the right user for this thread?
                     if os.environ.get("TEST_ENV") == "True":
                         print("Safetychecks passed for continue_falcon")
@@ -132,9 +116,7 @@ async def continue_falcon(message):
                     with open(full_generation, "r") as file:
                         data = json.load(file)
                         output_text = data[-1][-1]
-                    threadid_conversation[
-                        message.channel.id
-                    ] = full_generation  # overwrite the old file
+                    threadid_conversation[message.channel.id] = full_generation  # overwrite the old file
                     if len(output_text) > 1300:
                         output_text = (
                             output_text[:1300]
