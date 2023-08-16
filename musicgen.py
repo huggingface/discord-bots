1e00563e43268f9bb308db82f01565b967047583
lunarflu <70143200+lunarflu@users.noreply.github.com>
2023-08-16 13:29:16 +0200

    Update musicgen.py

--- musicgen.py
+++ musicgen.py
@@ -2,7 +2,6 @@ import os
 
 import discord
 from gradio_client import Client
-from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_audio
 
 MUSIC_CHANNEL_ID = 1140990231730987058  # real musicgen channel
 
@@ -23,14 +22,26 @@ async def music_create(ctx, prompt):
             await thread.send("Please wait for the song to finish generating before generating a new one!")
             job = musicgen.submit(prompt, api_name="/predict")
             while not job.done():
-                pass
-            input_file = job.outputs()[0]
-            output_file = f"/tmp/gradio/{ctx.author.id}.mp3"
-            ffmpeg_extract_audio(input_file, output_file)
-
-            with open(output_file, "rb") as file:
+              pass
+            files = job.outputs()
+            files = files[0]
+            audio = files[0]
+            video = files[1]
+  
+            with open(audio, "rb") as file:
                 discord_file = discord.File(file)
             await thread.send(file=discord_file)
+
+            with open(video, "rb") as file:
+                discord_file = discord.File(file)
+            await thread.send(file=discord_file)            
+
+            embed = discord.Embed()
+            embed.set_thumbnail(url="https://abs.twimg.com/icons/apple-touch-icon-192x192.png")
+            embed.add_field(name= "Twitter", 
+                            value="[Share it!](https://twitter.com/compose/tweet)", inline=True)
+
+            await thread.send(embed=embed)
 
     except Exception as e:
         print(f"musicgen Error: {e}")
