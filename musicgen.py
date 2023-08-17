72e29a0bede9ce484ed93e39f9a6b2eead678a27
lunarflu <70143200+lunarflu@users.noreply.github.com>
2023-08-17 15:16:18 +0200

    Update musicgen.py

--- musicgen.py
+++ musicgen.py
@@ -36,6 +36,8 @@ async def music_create(ctx, prompt):
                 message = await ctx.send(f"**{prompt}** - {ctx.author.mention}")
                 if len(prompt) > 99:
                     small_prompt = prompt[:99]
+                else:
+                    small_prompt = prompt
                 thread = await message.create_thread(name=small_prompt, auto_archive_duration=60)
 
                 await thread.send(
