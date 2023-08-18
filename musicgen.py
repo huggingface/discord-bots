9e5d88d9ef213be48289f3d67ed3e154d41495c3
lunarflu <70143200+lunarflu@users.noreply.github.com>
2023-08-18 15:43:58 +0200

    Send error message if queue full
    https://github.com/huggingface/discord-bots/issues/5

--- musicgen.py
+++ musicgen.py
@@ -1,25 +1,36 @@
-import asyncio
 import os
 
+import asyncio
 import discord
 from gradio_client import Client
 
-BOT_USER_ID = 1102236653545861151  # real
-MUSIC_CHANNEL_ID = 1140990231730987058  # real
 
-musicgen = Client("huggingface-projects/transformers-musicgen", hf_token=os.getenv("HF_TOKEN"))
+BOT_USER_ID = 1102236653545861151 # real
+MUSIC_CHANNEL_ID = 1140990231730987058  # real musicgen channel
+
+musicgen = Client(
+    "huggingface-projects/transformers-musicgen", hf_token=os.getenv("HF_TOKEN")
+)
 
 
 def music_create_job(prompt):
     """Generates music based on a given prompt"""
     try:
+        return_queue = None
         job = musicgen.submit(prompt, api_name="/predict")
         while not job.done():
-            pass
+            queue_size = job.status().queue_size
+            if queue_size is not None:
+                if return_queue is None:
+                    return_queue = queue_size
+                elif queue_size > return_queue:
+                    return_queue = queue_size
+
         files = job.outputs()
         files = files[0]
-
-        return files
+        if return_queue is None:
+            return_queue = 0
+        return [files, return_queue]
 
     except Exception as e:
         print(f"music_create_job Error: {e}")
@@ -38,7 +49,9 @@ async def music_create(ctx, prompt):
                     small_prompt = prompt[:99]
                 else:
                     small_prompt = prompt
-                thread = await message.create_thread(name=small_prompt, auto_archive_duration=60)
+                thread = await message.create_thread(
+                    name=small_prompt, auto_archive_duration=60
+                )
 
                 await thread.send(
                     "[DISCLAIMER: HuggingBot is a beta feature; The MusicGen"
@@ -48,9 +61,17 @@ async def music_create(ctx, prompt):
                     print("Running music_create_job...")
 
                 loop = asyncio.get_running_loop()
-                files = await loop.run_in_executor(None, music_create_job, prompt)
+                output_files = await loop.run_in_executor(
+                    None, music_create_job, prompt
+                )
 
-                audio, video = files[0], files[1]
+                queue_size = output_files[1]
+                if queue_size > 5:
+                    await thread.send(f"Queue is full! Queue size: {queue_size}")
+
+                media_files = output_files[0]
+                audio = media_files[0]
+                video = media_files[1]
 
                 with open(audio, "rb") as file:
                     discord_file = discord.File(file)
@@ -61,11 +82,13 @@ async def music_create(ctx, prompt):
                 await thread.send(file=discord_file)
 
                 embed = discord.Embed()
-                embed.set_thumbnail(url="https://abs.twimg.com/icons/apple-touch-icon-192x192.png")
+                embed.set_thumbnail(
+                    url="https://abs.twimg.com/icons/apple-touch-icon-192x192.png"
+                )
                 tweet1 = "https://twitter.com/intent/tweet?text="
                 tweet2 = "I%20generated%20this%20audio%20using%20MusicGen"
                 tweet3 = "%20in%20the%20%F0%9F%A4%97%20@huggingface%20Discord!"
-                tweet4 = "%0Ahf.co/join/discord%0A%0APrompt:%20"
+                tweet4 = "%0Ahf.co/join/discord%0APrompt:%20"
                 prompt = prompt.replace(" ", "%20")
                 intent_link = f"{tweet1}{tweet2}{tweet3}{tweet4}{prompt}"
                 embed.add_field(
