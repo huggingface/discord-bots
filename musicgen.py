25d38c02aec8c642f1d1afec80a06620ee46f138
lunarflu <70143200+lunarflu@users.noreply.github.com>
2023-08-18 16:38:13 +0200

    Update musicgen.py

--- musicgen.py
+++ musicgen.py
@@ -2,7 +2,9 @@ import os
 
 import asyncio
 import discord
+
 from gradio_client import Client
+from gradio_client.utils import QueueError
 
 
 BOT_USER_ID = 1102236653545861151  # real
@@ -14,21 +16,10 @@ musicgen = Client("huggingface-projects/transformers-musicgen", hf_token=os.gete
 def music_create_job(prompt):
     """Generates music based on a given prompt"""
     try:
-        return_queue = None
         job = musicgen.submit(prompt, api_name="/predict")
         while not job.done():
-            queue_size = job.status().queue_size
-            if queue_size is not None:
-                if return_queue is None:
-                    return_queue = queue_size
-                elif queue_size > return_queue:
-                    return_queue = queue_size
-
-        files = job.outputs()
-        files = files[0]
-        if return_queue is None:
-            return_queue = 0
-        return [files, return_queue]
+            pass
+        return job
 
     except Exception as e:
         print(f"music_create_job Error: {e}")
@@ -57,13 +48,17 @@ async def music_create(ctx, prompt):
                     print("Running music_create_job...")
 
                 loop = asyncio.get_running_loop()
-                output_files = await loop.run_in_executor(None, music_create_job, prompt)
+                job = await loop.run_in_executor(None, music_create_job, prompt)
 
-                queue_size = output_files[1]
-                if queue_size > 95:
-                    await thread.send(f"Queue is full! Queue size: {queue_size}")
+                try:
+                    job.result()
+                    files = job.outputs()
+                    media_files = files[0]
+                except QueueError:
+                    await thread.send(
+                        "The gradio space powering this bot is really busy! Please try again later!"
+                    )
 
-                media_files = output_files[0]
                 audio = media_files[0]
                 video = media_files[1]
 
