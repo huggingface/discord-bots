1368908b333dc0ca807a8bef387da34d6102121c
lunarflu <70143200+lunarflu@users.noreply.github.com>
2023-08-30 10:37:47 +0200

    Update deepfloydif.py

--- deepfloydif.py
+++ deepfloydif.py
@@ -102,10 +102,6 @@ async def deepfloydif_stage_1(ctx, prompt, client):
                 else:
                     small_prompt = prompt
                 thread = await message.create_thread(name=f"{small_prompt}", auto_archive_duration=60)
-                await thread.send(
-                    "[DISCLAIMER: HuggingBot is a **highly experimental** beta feature; Additional information on the"
-                    " DeepfloydIF model can be found here: https://huggingface.co/spaces/DeepFloyd/IF"
-                )
                 await thread.send(f"{ctx.author.mention} Generating images in thread, can take ~1 minute...")
 
                 loop = asyncio.get_running_loop()
